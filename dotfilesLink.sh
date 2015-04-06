aeba4a85bbbe91a0f61ce2809206e6dbc15d6572
tpywao <tpywao@gmail.com>
2015-04-07 02:05:04 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,12 +1,29 @@
-#! /bin/bash
+#! /bin/sh
 path=`readlink -f $0`
 dir_path=`dirname $path`
+# if ( $1 == NULL or $1 == '' )
+shell=${1:-$SHELL}
 
-ln -sf $dir_path/.vim ~/.vim
-ln -sf $dir_path/.vimrc ~/.vimrc
-ln -sf $dir_path/.bashrc ~/.bashrc
-ln -sf $dir_path/.bash_aliases ~/.bash_aliases
-ln -sf $dir_path/.bash_git ~/.bash_git
-ln -sf $dir_path/.gitconfig ~/.gitconfig
-ln -sf $dir_path/.gitignore ~/.gitignore
+case $shell in
+  *zsh )
+    ln -si $dir_path/.zshenv ~/.zshenv
+    ln -si $dir_path/.zshrc ~/.zshrc
+    # ln -si $dir_path/.zshrc.alias ~/.zshrc.alias
+    ;;
+  *bash )
+    ln -si $dir_path/.bashrc ~/.bashrc
+    ln -si $dir_path/.bash_aliases ~/.bash_aliases
+    ;;
+esac
+
+# vim
+# ln -sin $dir_path/.vim ~/.vim
+ln -si $dir_path/.vimrc ~/.vimrc
+
+# git
+ln -si $dir_path/.gitconfig ~/.gitconfig
+ln -si $dir_path/.gitignore ~/.gitignore
+
+# tmux
+ln -si $dir_path/.tmux.conf ~/.tmux.conf
 
