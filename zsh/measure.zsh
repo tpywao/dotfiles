02a6d2e91e9f48f6f7132c82c6219b595b650e3c
tpywao <tpywao@gmail.com>
2017-04-24 21:28:41 +0900

    Add measure for zshenv zshrc

---
+++ zsh/measure.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+set_measure() {
+  echo "Turn on measure for zsh."
+
+  cp .zshrc .zshrc.org
+  cp .zshenv .zshenv.org
+  echo 'zmodload zsh/zprof && zprof' | cat - .zshenv.org > .zshenv
+  echo -e "if(type zporf > /dev/null); then\n  zprof\nfi" >> .zshrc
+}
+
+unset_measure() {
+  echo "Turn off measure for zsh."
+
+  mv .zshrc.org .zshrc
+  mv .zshenv.org .zshenv
+  rm .zshrc.org .zshenv.org
+}
+
+if [ $# = 1 ]; then
+  if [ $1 = "on" ]; then
+    set_measure
+  elif [ $1 = "off" ]; then
+    unset_measure
+  else
+    echo "Please, input 'on' or 'off'."
+  fi
+else
+  echo "Please, input one operator."
+fi
+
