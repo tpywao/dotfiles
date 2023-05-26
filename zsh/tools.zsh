a937c17ad74e7c2d41cfb28f9e3a6852177fe164
tpywao <tpywao@gmail.com>
2023-05-26 19:03:26 +0900

    Comment bitwardenに関するコメントを追加

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -11,6 +11,9 @@
 # rectangle
 # alacritty or warp
 
+# bitwardenはsafariに拡張機能を設定したい場合、app store経由でインストールする必要がある
+# https://bitwarden.com/help/install-safari-app-extension/
+
 if is_mac; then
   if !is_cmd_exists brew; then
     echo 'need `brew`' 1>&2
